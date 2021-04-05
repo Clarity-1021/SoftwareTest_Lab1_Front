107850a742de8323f2467c14404a6bb6ecd94b48
wendy <798098945@qq.com>
2021-04-05 18:51:04 +0800

    remove useless icon button

--- src/components/Lab1/Component/Normal/TopToolBar.vue
+++ src/components/Lab1/Component/Normal/TopToolBar.vue
@@ -6,13 +6,13 @@
 
         <v-spacer></v-spacer>
 
-        <v-btn icon>
-            <v-icon>mdi-heart</v-icon>
-        </v-btn>
+<!--        <v-btn icon>-->
+<!--            <v-icon>mdi-heart</v-icon>-->
+<!--        </v-btn>-->
 
-        <v-btn icon>
-            <v-icon>mdi-magnify</v-icon>
-        </v-btn>
+<!--        <v-btn icon>-->
+<!--            <v-icon>mdi-magnify</v-icon>-->
+<!--        </v-btn>-->
 
         <v-menu offset-y>
             <template v-slot:activator="{ on, attrs }">
